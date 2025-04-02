8bc779e8bd4d5f10f8da6c6767fe4a18b6e05c62
amber-r-edwards <arose52400@gmail.com>
2025-04-01 20:14:26 -0400

    test plot

--- babe.r
+++ babe.r
@@ -7,3 +7,11 @@ library(SnowballC)
 metadata <- read.csv("babemetadata.csv", sep = ",", header = TRUE)
 events <- read.csv("babeevents.csv", sep = ",", header = TRUE)
 resources <- read.csv("baberesources.csv", sep = ",", header = TRUE)
+
+#test plot - protests by months of 1970
+protestsbyymonth70 <- events %>%
+    filter(event.type == "Protest Report", event.year == 1970) %>%
+    group_by(event.month) %>%
+    summarize(count = n())
+
+ggplot(protestsbyymonth70, aes(x = event.month, y = count)) + geom_col()
